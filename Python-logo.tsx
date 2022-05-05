c4cdc85c7687a038ee19a7a921bf2f673180899f
hlepp <haleymlepp@gmail.com>
2022-05-04 17:14:04 -0700

    add new logos

---
+++ Python-logo.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Python-logo" tilewidth="32" tileheight="32" tilecount="3600" columns="60">
+ <image source="tiles/python.png" width="1920" height="1920"/>
+</tileset>
